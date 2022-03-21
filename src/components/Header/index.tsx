98b924c8e63d5a6b4f8e8a72eff83d17d9210d4e
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2022-03-20 21:18:54 -0300

    Trabalhando com CMS

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
+import { ActiveLink } from '../ActiveLink'
 import { SignInButton } from '../SignInButton'
+
 import styles from './styles.module.scss'
 
 export function Header() {
@@ -8,10 +10,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} href="#">
-            Home
-          </a>
-          <a href="#">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
